d8dc0a5d15ea40f0bd102fc77876560fe0b3091e
VietNga131 <161239556+VietNga131@users.noreply.github.com>
2026-05-23 22:50:11 +0700

    fix favorite list

--- backend/src/main/java/com/example/Rental/repository/FavoriteRepository.java
+++ backend/src/main/java/com/example/Rental/repository/FavoriteRepository.java
@@ -3,6 +3,7 @@ package com.example.Rental.repository;
 import com.example.Rental.entity.Favorite;
 import com.example.Rental.entity.FavoriteId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
 
@@ -10,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
+    @EntityGraph(attributePaths = {"post", "post.room", "post.room.images"})
     Page<Favorite> findByUserId(Long userId, Pageable pageable);
+
     boolean existsByUserIdAndPostId(Long userId, Long postId);
 }
