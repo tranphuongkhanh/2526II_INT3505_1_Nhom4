6264d0868509d31d6151f885cef26cf4638ba288
tranphuongkhanh <161273978+tranphuongkhanh@users.noreply.github.com>
2026-05-26 14:51:34 +0700

    refactor: add filter for room

--- backend/src/main/java/com/example/Rental/dto/request/RoomFilterRequest.java
+++ backend/src/main/java/com/example/Rental/dto/request/RoomFilterRequest.java
@@ -2,6 +2,7 @@ package com.example.Rental.dto.request;
 
 import com.example.Rental.enums.RentalStatus;
 import com.example.Rental.enums.RoomType;
+import jakarta.persistence.Column;
 import lombok.Data;
 import java.math.BigDecimal;
 
@@ -20,12 +21,17 @@ public class RoomFilterRequest {
     private String district;
     private String city;
 
-    private Boolean hasWifi;      // Tiện ích (Ví dụ)
     private Boolean hasAc;
     private Boolean hasFridge;
-    private Boolean hasParking;
     private Boolean hasPrivateWc;
     private Boolean hasSecurity;
+
+    private BigDecimal minWifiFee;
+    private BigDecimal minWaterPricePerUnit;
+    private BigDecimal minElectricityPricePerUnit;
+    private BigDecimal minServiceFee;
+    private BigDecimal minBikeParkingFee;
+    private BigDecimal minDeposit;
 
     private Double minAvgRating;  // Rating tối thiểu
 }
