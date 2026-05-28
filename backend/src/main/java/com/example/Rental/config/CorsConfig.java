fdcd5c5d555108927c76a79993851013d6091975
HoangNgocNhi <23020130@vnu.edu.vn>
2026-05-28 23:41:27 +0700

    feat: configure CORS policy for API endpoints to allow cross-origin requests

--- backend/src/main/java/com/example/Rental/config/CorsConfig.java
+++ backend/src/main/java/com/example/Rental/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://*.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://*.vercel.app", "http://100.27.50.216:30000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
